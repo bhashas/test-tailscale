cae115043f83010e9afaafd7038d7fa1695b825a
bhashas <116110608+bhashas@users.noreply.github.com>
2026-03-18 07:49:39 +0100

    Refactor Proxmox VM resource for provider version 3.x
    Updated Proxmox VM resource configuration to align with provider version 3.x specifications.

--- main.tf
+++ main.tf
@@ -1,41 +1,29 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "telmate/proxmox"
-      version = "3.0.1-rc3"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url          = "https://100.108.39.48:8006/api2/json"
-  pm_api_token_id     = var.pm_api_token_id
-  pm_api_token_secret = var.pm_api_token_secret
-  pm_tls_insecure     = true
-}
-
 resource "proxmox_vm_qemu" "vm_test" {
   name        = "vm-test-tailscale"
   target_node = "pve-1"
-  vmid        = 505 # ID libre
+  vmid        = 505
 
-  clone       = "ubuntu-22.04-template" # Ton template ID 9000
+  clone       = "ubuntu-22.04-template"
   full_clone  = true
 
   cores   = 1
   memory  = 1024
-  
+  cpu     = "host"
+
   network {
     model  = "virtio"
     bridge = "vmbr0"
   }
 
-  disk {
-    size    = "10G"
-    type    = "scsi"
-    storage = "local-lvm"
+  # Voici la correction pour la version 3.x du provider
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          size    = "10G"
+          storage = "local-lvm"
+        }
+      }
+    }
   }
 }
-
-variable "pm_api_token_id" {}
-variable "pm_api_token_secret" {}
